17845d2922908d59fad6ac33d699fa186a9871ca
Michael Behrisch <oss@behrisch.de>
2026-06-20 12:45:03 +0200

    using cmake build on linux as well

--- recipes/fox/all/conanfile.py
+++ recipes/fox/all/conanfile.py
@@ -1,11 +1,6 @@
 from conan import ConanFile
-from conan.tools.apple import fix_apple_shared_install_name
-from conan.tools.build import cross_building
-from conan.tools.env import VirtualRunEnv
-from conan.tools.files import apply_conandata_patches, copy, get, rm, rmdir
+from conan.tools.files import copy, get
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.gnu import Autotools, AutotoolsDeps, AutotoolsToolchain
-from conan.tools.layout import basic_layout
 import os
 
 
@@ -49,10 +44,7 @@ class PackageConan(ConanFile):
                     os.path.join(self.export_sources_folder, dir))
 
     def layout(self):
-        if self.settings.os == "Windows":
-            cmake_layout(self)
-        else:
-            basic_layout(self, src_folder="src")
+        cmake_layout(self)
 
     def requirements(self):
         # Prefer self.requirements() method instead of self.requires attribute.
@@ -65,68 +57,26 @@ class PackageConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        # apply patches listed in conandata.yml
-        # Using patches is always the last resort to fix issues. If possible, try to fix the issue in the upstream project.
-        apply_conandata_patches(self)
 
     def generate(self):
-        if self.settings.os == "Windows":
-            CMakeToolchain(self).generate()
-            CMakeDeps(self).generate()
-            return
-        # inject required env vars into the build scope
-        # it's required in case of native build when there is AutotoolsDeps & at least one dependency which might be shared, because configure tries to run a test executable
-        if not cross_building(self):
-            VirtualRunEnv(self).generate(scope="build")
-        # --fpic is automatically managed when 'fPIC' option is declared
-        # --enable/disable-shared is automatically managed when 'shared' option is declared
-        tc = AutotoolsToolchain(self)
-        # autotools usually uses 'yes' and 'no' to enable/disable options
-        def yes_no(v): return "yes" if v else "no"
-        tc.configure_args.extend([
-            f"--enable-jpeg={yes_no(self.options.with_jpeg)}",
-        ])
-        tc.generate()
-        # generate dependencies for autotools
-        # some recipes might require a workaround for MSVC (https://github.com/conan-io/conan/issues/12784):
-        # https://github.com/conan-io/conan-center-index/blob/00ce907b910d0d772f1c73bb699971c141c423c1/recipes/xapian-core/all/conanfile.py#L106-L135
-        deps = AutotoolsDeps(self)
-        deps.generate()
+        CMakeToolchain(self).generate()
+        CMakeDeps(self).generate()
 
     def build(self):
-        if self.settings.os == "Windows":
-            for dir in (".", "src", "utils"):
-                copy(self, "CMakeLists.txt", os.path.join(self.export_sources_folder, dir),
-                     os.path.join(self.source_folder, dir))
-            cmake = CMake(self)
-            cmake.configure()
-            cmake.build()
-        else:
-            autotools = Autotools(self)
-            autotools.configure()
-            autotools.make()
+        for dir in (".", "src", "utils"):
+            copy(self, "CMakeLists.txt", os.path.join(self.export_sources_folder, dir),
+                 os.path.join(self.source_folder, dir))
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def package(self):
         copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
-        if self.settings.os == "Windows":
-            cmake = CMake(self)
-            cmake.install()
-            return
-
-        autotools = Autotools(self)
-        autotools.install()
-
-        # Some files extensions and folders are not allowed. Please, read the FAQs to get informed.
-        rm(self, "*.la", os.path.join(self.package_folder, "lib"))
-        # Consider disabling these at first to verify that the package_info() output matches the info exported by the project.
-        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
-        rmdir(self, os.path.join(self.package_folder, "share"))
-
-        # In shared lib/executable files, autotools set install_name (macOS) to lib dir absolute path instead of @rpath, it's not relocatable, so fix it
-        fix_apple_shared_install_name(self)
+        cmake = CMake(self)
+        cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["FOX-1.6"]              # libFOX-1.6.so / FOX-1.6.lib
+        self.cpp_info.libs = ["fox-1.6"]                 # libfox-1.6.so / fox-1.6.lib
         self.cpp_info.includedirs = ["include/fox-1.6"]  # FOX installs headers here, not include/
 
         if self.settings.os in ["Linux", "FreeBSD"]:
